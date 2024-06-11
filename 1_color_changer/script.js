ae870a300d0de881d432dff4f3000cf7f6163ce4
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2024-06-11 17:00:06 +0530

    project 1 completed

--- 1_color_changer/script.js
+++ 1_color_changer/script.js
@@ -27,15 +27,3 @@ buttons.forEach((button) => {
     }
   });
 });
-
-// button.addEventListener("click", (e) => {
-//   console.log(e);
-//   console.log(e.target);
-// });
-
-{
-  {
-    {
-    }
-  }
-}
