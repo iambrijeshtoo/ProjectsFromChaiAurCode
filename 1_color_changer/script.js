71a182ac07156ab5056ee9239f8a101e43212bfc
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2024-06-11 16:57:17 +0530

    feat: add color switcher logic

--- 1_color_changer/script.js
+++ 1_color_changer/script.js
@@ -2,8 +2,40 @@ const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    console.log(e);
-    console.log(e.target);
+  // console.log(button);
+  button.addEventListener("click", (eventObject) => {
+    // console.log(eventObject);
+    // console.log(eventObject.target);
+    switch (eventObject.target.id) {
+      case "green":
+        body.style.backgroundColor = "#04e762";
+        break;
+      case "yellow":
+        body.style.backgroundColor = "#f5b700";
+        break;
+      case "blue":
+        body.style.backgroundColor = "#00a1e4";
+        break;
+      case "pink":
+        body.style.backgroundColor = "#dc0073";
+        break;
+      case "light-green":
+        body.style.backgroundColor = "#89fc00";
+        break;
+      default:
+        break;
+    }
   });
 });
+
+// button.addEventListener("click", (e) => {
+//   console.log(e);
+//   console.log(e.target);
+// });
+
+{
+  {
+    {
+    }
+  }
+}
